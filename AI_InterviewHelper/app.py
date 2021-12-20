2fc5b37201d15dbf31ace6d036c6d3aceb11a226
HojinMoon <phantomhojin@naver.com>
2021-12-20 18:55:12 +0900

    popup size updated

--- AI_InterviewHelper/app.py
+++ AI_InterviewHelper/app.py
@@ -19,8 +19,8 @@ def connectsql():
                     host='localhost',
                     port = 3306,
                     user='root',
-                    passwd='hojin0215!',
-                    db = 'userlist',
+                    passwd='tiger',
+                    db = 'mypage',
                     charset='utf8')
     return db
 
